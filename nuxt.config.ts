fcac41d44e323d18c6d309a925ad798215e9577d
akbarsyn <akbar.p.suryamin@gli.id>
2025-11-09 09:16:15 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@vite-pwa/nuxt'
+  ],
+  pwa: {
+    manifest: {
+      name: "Shared ledger app",
+      short_name: 'SL APP',
+      description: "this is my idea app for shared ledger"
+    },
+    workbox: {
+      navigateFallback: '/'
+    },
+    devOptions: {
+      enabled: true,
+      type: "module"
+    }
+  }
+
+})
