1ee4fca6c87611f5a73c10f3fdfc329aa847069d
akbarsyn <akbar.p.suryamin@gli.id>
2025-11-14 00:29:00 +0700

    feat(dashboard): add dashboard index page

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Dashboard</h1>
+  <p v-if="isVisible">{{ message }}</p>
+  <p>{{ userInfo?.name }}</p>
+</template>
+
+<script setup lang="ts">
+definePageMeta({ middleware: ["auth"] });
+const { userInfo } = useAuthStore();
+const { message, isVisible } = useNotifier();
+</script>
